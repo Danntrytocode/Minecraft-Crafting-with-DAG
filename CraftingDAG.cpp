267f194ab84dfab79fc2cc70decb645c0ee434ee
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-06-15 21:10:45 +0700

    inisialisasi kelas CraftingDag dengan member variabel yang di butuhkan

--- CraftingDAG.cpp
+++ CraftingDAG.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 
 using namespace std;
+
+class CraftingDAG {
+private:
+    string namaItem[10];//Array penyimpan nama bahan
+    int hubMatrix[10][10];//Matriks Ketetanggaan (Arah Graf)
+    int inDegree[10];//Array penyimpan jumlah prasyarat (panah masuk)
+    int jumlahItem;
+    
+    //Variabel untuk Queue
+    int antrean[10];
+    int front;
+    int rear;
+};
